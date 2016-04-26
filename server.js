1b0fe8d4d02521a18b4829ea80e7718267ab6568
Eric Humpert <EricMac@ricks-mbp.apollosrc.root>
2016-04-26 14:01:42 -0500

    made some minor progress so far with the header nav list.

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-  console.log('App is running on port' + 3000);
+app.listen(port, function() {
+  console.log('App is running on port ' + port);
 })
